9db69db56e1fb45f95bfafbed64531b570353da9
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-28 18:42:37 -0300

    requisito 2 concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const especies = data.species;
+
+const getAnimalsOlderThan = (animal, age) => {
+  const item = especies.find((animais) => animais.name === animal);
+  return item.residents.every((element) => element.age > age);
+};
 
 module.exports = getAnimalsOlderThan;
