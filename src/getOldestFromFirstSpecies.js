91d91bc11ebc151a81a3183dc7b2afbd24e5b86c
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-29 18:31:16 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const funcionarios = data.employees;
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const animais = data.species;
+
+const getSpecies = (id) =>
+  animais.find((element) => element.id === id);
+
+const getAnimalBySex = (genero, busca) => {
+  const animal = busca.residents.filter((item) => item.sex === genero);
+  const animalOlder = animal.reduce((acc, curr) => {
+    if (acc.age > curr.age) return acc;
+    return curr;
+  });
+  return Object.values(animalOlder);
+};
+
+const getOldestFromFirstSpecies = (id, gen) => {
+  const funcionario = funcionarios.find((pessoa) => pessoa.id === id);
+  const firstAnimal = funcionario.responsibleFor[0];
+  const especie = getSpecies(firstAnimal);
+  if (!gen) {
+    const animalMix = especie.residents.reduce((acc, curr) => {
+      if (acc.age > curr.age) return acc;
+      return curr;
+    });
+    return Object.values(animalMix);
+  }
+  getAnimalBySex(gen, especie);
+};
+
+console.log(getOldestFromFirstSpecies(stephanieId, 'male'));
 
 module.exports = getOldestFromFirstSpecies;
