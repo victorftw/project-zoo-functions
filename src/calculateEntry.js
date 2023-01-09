ba283e28203db0cdbe50d69f2d0bea4b8745ea67
Victor Hugo <victor.hiraiftw@outlook.com>
2023-01-08 21:10:03 -0300

    feat: requisito 14 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,17 +3,19 @@ const data = require('../data/zoo_data');
 const { adult, senior, child } = data.prices;
 
 const countEntrants = (entrants) =>
-  entrants.reduce((acc, cliente) => {
-    if (cliente.age < 18) {
-      acc.child += 1;
-    } else if (cliente.age >= 18 && cliente.age < 50) {
-      acc.adult += 1;
-    } else {
-      acc.senior += 1;
-    }
-    return acc;
-  },
-  { child: 0, adult: 0, senior: 0 });
+  entrants.reduce(
+    (acc, cliente) => {
+      if (cliente.age < 18) {
+        acc.child += 1;
+      } else if (cliente.age >= 18 && cliente.age < 50) {
+        acc.adult += 1;
+      } else {
+        acc.senior += 1;
+      }
+      return acc;
+    },
+    { child: 0, adult: 0, senior: 0 },
+  );
 
 const calculateEntry = (entrants) => {
   if (typeof entrants === 'undefined') {
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const { species } = data;
+
+const filterBySex = (residents, options) => {
+  const { sex } = options;
+
+  if (!sex) {
+    return residents;
+  }
+
+  return residents.filter((resident) => resident.sex === sex);
+};
+
+const sortByName = (residents, options) => {
+  const { sorted } = options;
+
+  if (!sorted) {
+    return residents;
+  }
+
+  return residents.sort();
+};
+
+const getAnimalMap = (options = {}) => {
+  const animals = species.reduce((acc, animal) => {
+    const obj = acc;
+    if (!obj[animal.location]) obj[animal.location] = [];
+    if (options.includeNames) {
+      let { residents } = animal;
+
+      residents = filterBySex(residents, options);
+
+      let onlyNames = residents.map((a) => a.name);
+      onlyNames = sortByName(onlyNames, options);
+
+      obj[animal.location].push({ [animal.name]: onlyNames });
+    } else {
+      obj[animal.location].push(animal.name);
+    }
+    return obj;
+  }, {});
+  return animals;
+};
 
 module.exports = getAnimalMap;
